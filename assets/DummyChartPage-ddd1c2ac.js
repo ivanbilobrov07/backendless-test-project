4f5cd607e3e235c9c41ddaac66b1e134516ff8b6
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-10-27 01:08:54 +0300

    Updates

---
+++ assets/DummyChartPage-ddd1c2ac.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-35901346.js";const e=()=>r.jsx("table",{children:r.jsxs("tr",{children:[r.jsx("td",{children:"Dummy"}),r.jsx("td",{children:"Chart"})]})});export{e as default};
--- assets/DummyChartPage-fb5a8ee1.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-7c2d229b.js";const e=()=>r.jsx("table",{children:r.jsxs("tr",{children:[r.jsx("td",{children:"Dummy"}),r.jsx("td",{children:"Chart"})]})});export{e as default};
