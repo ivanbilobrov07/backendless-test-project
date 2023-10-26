fabf98855cea625d7348e22ffe7c443318d40d42
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-10-27 00:54:25 +0300

    Updates

---
+++ assets/DummyChartPage-d5773c18.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-89d38d39.js";const e=()=>r.jsx("table",{children:r.jsxs("tr",{children:[r.jsx("td",{children:"Dummy"}),r.jsx("td",{children:"Chart"})]})});export{e as default};
