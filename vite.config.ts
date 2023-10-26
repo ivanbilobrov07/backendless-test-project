2fa6ca33596d46642fabc5c5386e9bea8739aa6f
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-10-27 00:58:29 +0300

    fix gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/backendless-test-project",
+  base: "./backendless-test-project",
   plugins: [react()],
 });
