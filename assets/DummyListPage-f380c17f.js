9392894290fce76a3ae9a292c39e584012721faa
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-10-27 01:41:26 +0300

    Updates

---
+++ assets/DummyListPage-f380c17f.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-894a1407.js";const r=()=>t.jsx("table",{children:t.jsxs("tr",{children:[t.jsx("td",{children:"Dummy"}),t.jsx("td",{children:"List"})]})});export{r as default};
--- assets/DummyTablePage-cd6e7c65.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-2ef5b10f.js";const t=()=>e.jsx("table",{children:e.jsxs("tr",{children:[e.jsx("td",{children:"Dummy"}),e.jsx("td",{children:"Table"})]})});export{t as default};
