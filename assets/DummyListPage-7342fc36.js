b321d3c2d90f9d6460840e72a55d4627dbe6e929
ivanbilobrov07 <ivanbilobrov07@users.noreply.github.com>
2023-10-27 10:16:52 +0000

    deploy: c17f16651fddde944e70843189756dbd915a8784

--- assets/DummyListPage-7342fc36.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-4e6c419b.js";const r=()=>t.jsx("table",{children:t.jsxs("tr",{children:[t.jsx("td",{children:"Dummy"}),t.jsx("td",{children:"Listt"})]})});export{r as default};
---
+++ assets/DummyTablePage-cbb3d41c.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-ff1a45ba.js";const t=()=>e.jsx("table",{children:e.jsxs("tr",{children:[e.jsx("td",{children:"Dummy"}),e.jsx("td",{children:"Table"})]})});export{t as default};
